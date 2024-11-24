8debab2c2af61e13c118f7b17626dc25e01206b9
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2024-11-24 19:05:09 +0530

    feat/ SSR deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
   images: { unoptimized: true },
 };
 
-module.exports = nextConfig;
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
+
+export default nextConfig;
